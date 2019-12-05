f2dbfbabd1d3716836e31a042e65e2ea607a9420
Andrew Ross <andrew.simpson.ross@gmail.com>
2019-12-05 08:33:59 -0600

    Array modifications

--- script.js
+++ script.js
@@ -115,16 +115,15 @@ function inputSearch() {
 
 inputSearch();
 const cityArray = [
-
-];
-/*    "Atlanta",
+    "Atlanta",
     "Austin",
     "Chicago",
     "Denver",
     "New York",
     "Orlando",
     "San Francisco",
-    "Seattle"*/
+    "Seattle"
+];
 
 //creating city function to cycle through cityArray elements and retrieve city data upon click event
 let cityList = document.getElementById("cityList");
